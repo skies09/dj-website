9f8050b6c9e47a2039a19dc79235b93ca2bcd122
Donna <skies09@hotmail.com>
2019-08-02 13:26:19 +0000

    updating routing

---
+++ assets/js/javascript.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+ 
+//sets current h2 to change color and font-size on mouse enter
+$(".name-header").click(function(){
+	$(this).css('background-color','red');
+	$(this).css('font-size','20px');	 
+});
+});
